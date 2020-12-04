1ed0e83d2d5bf96635b34d0f8b89ab3aefa9fe88
Patrick Boren <Pboren@lcs.on.ca>
2020-12-04 09:26:10 -0500

    i finished drawing an arrow with the tortoise format

---
+++ Playground.playground/Pages/tesselation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,85 @@
+//: [Previous](@previous) / [Next](@next)
+//: # Blank canvas to copy
+//:
+//: ## To duplicate this page
+//:
+//: Place your cursor on line 19 of this page, press `Command-A` to select all text, and then `Command-C` to copy.
+//:
+//: Two-finger tap on the playground, then choose **New Playground Page**:
+//:
+//: ![new-playground](new-playground.png "New Playground")
+//:
+//: Move to the new page and press `Command-A` to select all text there, then `Command-V` to paste.
+//:
+/*:
+ ## Canvas size
+ 
+ Set the size of your desired canvas by adjusting the constants on lines 19 and 20.
+ */
+let preferredWidth = 300
+let preferredHeight = 600
+/*:
+ ## Required code
+ 
+ Lines 28 to 36 are required to make the playground run.
+ 
+ Please do not remove.
+ */
+import Cocoa
+import PlaygroundSupport
+import CanvasGraphics
+
+// Create canvas
+let canvas = Canvas(width: 500, height: 500)
+
+// Show the canvas in the playground's live view
+PlaygroundPage.current.liveView = canvas
+/*:
+ ## Add your code
+ 
+ Beginning on line 48, write a meaningful comment.
+ 
+ You can remove the code on line 49 and begin writing your own code.
+ 
+ [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
+
+ */
+
+//create a turtle to use
+
+let turtle = Tortoise(drawingUpon: canvas)
+
+turtle.penUp()
+turtle.left(by: 90)
+turtle.forward(steps: 200)
+turtle.right(by: 90)
+turtle.forward(steps: 125)
+turtle.penDown()
+turtle.forward(steps: 150)
+turtle.right(by: 90)
+turtle.forward(steps: 70)
+turtle.left(by: 130)
+turtle.forward(steps: 160)
+turtle.left(by: 100)
+turtle.forward(steps: 160)
+turtle.left(by: 130)
+turtle.forward(steps: 70)
+turtle.right(by: 90)
+turtle.forward(steps: 150)
+turtle.left(by: 90)
+turtle.forward(steps: 65)
+/*:
+ ## Show the Assistant Editor
+ Don't see any results?
+ 
+ Remember to show the Assistant Editor (1), and then switch to Live View (2):
+ 
+ ![timeline](timeline.png "Timeline")
+
+ ## Use source control
+ To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
+ 
+ Please commit and push your work often.
+ 
+ ![source_control](source-control.png "Source Control")
+ */
